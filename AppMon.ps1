6005ce3e84cd15624e7ee455b8046b62265bf9b3
DaybetLee <55645717+DaybetLee@users.noreply.github.com>
2022-05-20 16:17:28 +0800

    Add Script via upload.

---
+++ AppMon.ps1
@@ -0,0 +1,75 @@
+$notpressed = $true
+$string = 'Press any key to stop logging:'
+$dots = 1
+$i = 0
+
+$targetProcess = "FortiSSLVPNdaemon"
+$targetService = "FA_Scheduler"
+$targetPort = 25427
+
+# Create ps_logs folder in Desktop if it is not already there
+if (-not (Test-Path -Path $env:HOMEPATH\Desktop\ps_logs)) {
+    # Out-Null prevent output to console
+    New-Item -Path "$env:HOMEPATH\Desktop" -Name "ps_logs" -ItemType "directory" | Out-Null
+}
+
+function LogNow {
+    # Check if current process
+    if (Get-Process -Name $targetProcess -ErrorAction SilentlyContinue) {
+        # "$(Get-Date -format 'u') - Info - $targetProcess is running. Wait for next minute to check again." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+    }
+    else {
+        "$(Get-Date -format 'u') - Err - $targetProcess is not running. Initial capturing." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+
+        # Capture application logs
+        $DateStamp = get-date -uformat "%Y-%m-%d@%H-%M-%S"
+        Copy-Item -Path "$env:ProgramData\Aruba Networks\ClearPassOnGuard" -Destination "$env:HOMEPATH\Desktop\ps_logs\ProgramData-$DateStamp" -Recurse
+        Copy-Item -Path "$env:APPDATA\Aruba Networks\ClearPassOnGuard" -Destination "$env:HOMEPATH\Desktop\ps_logs\APPDATA-$DateStamp" -Recurse
+        "$(Get-Date -format 'u') - Info - Copied %AppData%\Aruba Networks\ClearPassOnGuard and %ProgramData%\Aruba Networks\ClearPassOnGuard logs." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+
+        # Check if local port uses the port
+        if (Get-NetTCPConnection -LocalPort $targetPort -ErrorAction SilentlyContinue ) {
+            "$(Get-Date -format 'u') - Info - Capturing application that uses local port $targetPort" | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+            Get-NetTCPConnection -LocalPort $targetPort | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+        }
+        else {
+            "$(Get-Date -format 'u') - Info - Local Port $targetPort is not in use." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+        }
+
+        # Capture Windows Event Logs
+        Get-EventLog -LogName System -Newest 5 | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+        "$(Get-Date -format 'u') - Info - Captured Event Logs." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+        
+        # Restart Application Service
+        "$(Get-Date -format 'u') - Info - Capturing the 'ClearPass Agent Controller' service status before restart." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+        Get-Service $targetService | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+        "$(Get-Date -format 'u') - Info - Restarting 'ClearPass Agent Controller' service." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+        Start-Service -InputObject $targetService  -PassThru | Format-List >> $env:HOMEPATH\Desktop\ps_logs\logging.txt
+     
+        Write-Host $string
+    }
+}
+
+# Delay per check
+Write-Host $string
+while ($notpressed) {
+    $i++
+    if ([console]::KeyAvailable) {
+        $notpressed = $false    
+    }    
+    else {
+        if ($i % ($dots + 1) -eq 1) {
+            # Write-Host $string -NoNewline
+            Start-Sleep -Milliseconds 500          
+        }
+        elseif ($i % ($dots + 1) -eq 0) {
+            # Write-Host 'logged'
+            LogNow
+            Start-Sleep -Milliseconds 500           
+        }
+        else {
+            # Write-Host '.' -NoNewline
+            Start-Sleep -Milliseconds 1000           
+        }
+    }
+}
