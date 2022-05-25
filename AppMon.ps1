ac51c3b85019a8d7b5601a6e8e2e209986680cb4
DaybetLee <55645717+DaybetLee@users.noreply.github.com>
2022-05-25 14:00:47 +0800

    Update AppMon.ps1

--- AppMon.ps1
+++ AppMon.ps1
@@ -11,48 +11,48 @@ $progamPath = "\Aruba Networks\ClearPassOnGuard"
 $appdataPath = "\Aruba Networks\ClearPassOnGuard"
 
 # Create ps_logs folder in Desktop if it is not already there
-if (-not (Test-Path -Path $env:HOMEPATH\Desktop\ps_logs)) {
+if (-not (Test-Path -Path $env:USERPROFILE\Desktop\ps_logs)) {
     # Out-Null prevent output to console
-    New-Item -Path "$env:HOMEPATH\Desktop" -Name "ps_logs" -ItemType "directory" | Out-Null
+    New-Item -Path "$env:USERPROFILE\Desktop" -Name "ps_logs" -ItemType "directory" | Out-Null
 }
 
 function LogNow {
     # Check if current process
     if (Get-Process -Name $targetProcess -ErrorAction SilentlyContinue) {
-        # "$(Get-Date -format 'u') - Info - $targetProcess is running. Wait for next minute to check again." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+        # "$(Get-Date -format 'u') - Info - $targetProcess is running. Wait for next minute to check again." | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
     }
     else {
-        "$(Get-Date -format 'u') - Err - $targetProcess is not running. Initial capturing." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+        "$(Get-Date -format 'u') - Err - $targetProcess is not running. Initial capturing." | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
 
         # Capture application logs
         $DateStamp = get-date -uformat "%Y-%m-%d@%H-%M-%S"
-        Copy-Item -Path "$env:ProgramData$progamPath" -Destination "$env:HOMEPATH\Desktop\ps_logs\ProgramData-$DateStamp" -Recurse
-        Copy-Item -Path "$env:APPDATA$appdataPath" -Destination "$env:HOMEPATH\Desktop\ps_logs\APPDATA-$DateStamp" -Recurse
-        "$(Get-Date -format 'u') - Info - Copied %AppData%\Aruba Networks\ClearPassOnGuard and %ProgramData%\Aruba Networks\ClearPassOnGuard logs." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+        Copy-Item -Path "$env:ProgramData$progamPath" -Destination "$env:USERPROFILE\Desktop\ps_logs\ProgramData-$DateStamp" -Recurse
+        Copy-Item -Path "$env:APPDATA$appdataPath" -Destination "$env:USERPROFILE\Desktop\ps_logs\APPDATA-$DateStamp" -Recurse
+        "$(Get-Date -format 'u') - Info - Copied %AppData%\Aruba Networks\ClearPassOnGuard and %ProgramData%\Aruba Networks\ClearPassOnGuard logs." | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
 
         # Check if local port uses the port
         if (Get-NetTCPConnection -LocalPort $targetPort -ErrorAction SilentlyContinue ) {
-            "$(Get-Date -format 'u') - Info - Capturing application that uses local port $targetPort" | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
-            Get-Process -Id (Get-NetTCPConnection -LocalPort $targetPort).OwningProcess | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+            "$(Get-Date -format 'u') - Info - Capturing application that uses local port $targetPort" | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
+            Get-Process -Id (Get-NetTCPConnection -LocalPort $targetPort).OwningProcess | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
             
             if (Get-NetUDPEndpoint -LocalPort $targetPort -ErrorAction SilentlyContinue ) {
-                "$(Get-Date -format 'u') - Info - Capturing application that uses local port $targetPort" | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
-                Get-Process -Id (Get-NetUDPEndpoint -LocalPort $targetPort).OwningProcess | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+                "$(Get-Date -format 'u') - Info - Capturing application that uses local port $targetPort" | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
+                Get-Process -Id (Get-NetUDPEndpoint -LocalPort $targetPort).OwningProcess | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
             }
         }
         else {
-            "$(Get-Date -format 'u') - Info - Local Port $targetPort is not in use." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+            "$(Get-Date -format 'u') - Info - Local Port $targetPort is not in use." | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
         }
 
         # Capture Windows Event Logs
-        Get-EventLog -LogName System -Newest 5 | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
-        "$(Get-Date -format 'u') - Info - Captured Event Logs." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
+        Get-EventLog -LogName System -Newest 5 | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
+        "$(Get-Date -format 'u') - Info - Captured Event Logs." | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
         
         # Restart Application Service
-        "$(Get-Date -format 'u') - Info - Capturing the 'ClearPass Agent Controller' service status before restart." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
-        Get-Service $targetService | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
-        "$(Get-Date -format 'u') - Info - Restarting 'ClearPass Agent Controller' service." | Out-File -FilePath $env:HOMEPATH\Desktop\ps_logs\logging.txt -Append
-        Start-Service -InputObject $targetService  -PassThru | Format-List >> $env:HOMEPATH\Desktop\ps_logs\logging.txt
+        "$(Get-Date -format 'u') - Info - Capturing the 'ClearPass Agent Controller' service status before restart." | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
+        Get-Service $targetService | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
+        "$(Get-Date -format 'u') - Info - Restarting 'ClearPass Agent Controller' service." | Out-File -FilePath $env:USERPROFILE\Desktop\ps_logs\logging.txt -Append
+        Start-Service -InputObject $targetService  -PassThru | Format-List >> $env:USERPROFILE\Desktop\ps_logs\logging.txt
      
         Write-Host $string
     }
